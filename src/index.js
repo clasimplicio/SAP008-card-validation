75580e07e917ecb22c16f59cfae4ef6d401769a9
Clarissa Simplício <clasimplicio@hotmail.com>
2022-07-20 17:04:02 -0300

    funções funcionando

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import validator from './validator.js';
 
+//--------------ADICIONANDO EVENTO AO BOTÃO VALIDAR-----------------
+document.getElementById("ok").addEventListener("click",pegar);
+
+let creditCardNumber;
+//--------------FUNÇÃO DO BOTÃO VALIDAR---------------------------------
+function pegar() {
+    //-------------------RECEBE O Q FOI DIGITADO NO INPUT---------------
+    creditCardNumber = document.getElementById("card").value;
+    //-----------CHAMANDO O MÉTODO DA OBJETO VALIDATOR---------------------
+    let valid = validator.isValid(creditCardNumber);
+    //-------------------------VERIFICANDO A VALIDAÇÃO COM O RETURNO NA VARIÁVEL VALID-----------------------
+    if(valid==true){
+        alert("CARTÃO VÁLIDO")
+    }else{
+        alert("CARTÃO INVÁLIDO")
+    }
+}
+
+//-----------------------ADICIONANDO EVENTO AO BOTÃO MASCARAR-------------------------------
+document.getElementById("mascara").addEventListener("click",camuflar);
+//------------------FUNÇÃO DO BOTÃO MASCARAR----------------------
+function camuflar(){
+    //--------------RECEBE O QUE FOI DIGITADO NO INPUT--------------------
+    creditCardNumber = document.getElementById("card").value;
+    //---------------------------CHAMA O METODO DO OBJETO VALIDATOR--------------------
+    validator.maskify(creditCardNumber);
+}
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,76 @@
 const validator = {
-  // ...
+    
+    isValid: (creditCardNumber) => {
+        //---------------STRING PRA ARRAY----------------------------
+        const arrayCardNumber = Array.from(creditCardNumber);
+        //-----------------SOMA FINAL PARA VALIDAÇÃO------------------------------
+        let soma=0;
+     
+        // console.log(arrayCardNumber);
+        //--------------------TAMANHO ARRAY---------------------------
+        let sizeCardNumber = arrayCardNumber.length; 
+        // console.log(sizeCardNumber)
+        // console.log(typeof(sizeCardNumber))
+        // alert("entrou")
+        for(let i=0; i<sizeCardNumber; i++){
+            //---------------------SE FOR PAR A QUANTIDADE DE NUMEROS DO CARTÃO-----------------------------------
+            if(sizeCardNumber%2===0){
+                if(i%2===0){
+                        //-----------------------DOBRANDO OS VALORES---------------------
+                        //---------------------STEP 1-----------------
+                        arrayCardNumber[i]=arrayCardNumber[i]*2;
+                        //---------------------PRA SABER SE O DOBRO DEU MAIOR QUE 9--------------------
+                        //--------------------STEP 2------------------
+                        if(arrayCardNumber[i]>9){
+                            arrayCardNumber[i]=arrayCardNumber[i]-9;
+                        }
+                    }else{
+                        //------------------TRANSFORMANDO EM NUMBER--------------------------
+                        arrayCardNumber[i]=arrayCardNumber[i]*1;
+                    }
+                //------------------------------ SE FOR IMPAR A QUANTIDADE DE NUMEROS DO CARTÃO-------------------    
+            }else{
+                if(i%2===0){
+                    //-----------TRANSFORMANDO EM NUMBER---------------
+                    arrayCardNumber[i]=arrayCardNumber[i]*1;
+                }else{
+                    //------------------------DOBRANDO OS VALORES
+                    //--------------------STEP 1---------------------------
+                    arrayCardNumber[i]=arrayCardNumber[i]*2;
+                    //------------------PRA SABER SE O DOBRO DEU MAIOR QUE 9-----------------------
+                    //-------------------------------STEP 2---------------------
+                    if(arrayCardNumber[i]>9){
+                        arrayCardNumber[i]=arrayCardNumber[i]-9;
+                    }
+                }
+            }
+            //-------SOMAR TODOS OS NUMEROS----------------
+            //-----------------STEP 3----------------------------  
+            soma+=arrayCardNumber[i];
+        }
+        //---------------FAZER A VALIDAÇÃO DO CARTÃO-------------------
+        if(soma%10===0){
+            return true;
+        }else{
+            return false;
+        }
+        //console.log(arrayCardNumber);   
+    }
+    ,
+    maskify:(creditCardNumber) => {
+        //---------------STRING PRA ARRAY----------------------------
+        const arrayCardNumber = Array.from(creditCardNumber);
+        //--------------------TAMANHO ARRAY---------------------------
+        let sizeCardNumber = arrayCardNumber.length; 
+        //---------------- ALTERA SE A QUANTIDADE DE NUMEROS DO CARTÃO FOR MAIOR 4-------------------
+        if(sizeCardNumber>4){
+            for(let i=0; i<(sizeCardNumber-4); i++){
+                arrayCardNumber[i]='#'
+            }
+        }
+        //-----------------IMPRIMINDO O ARRAY EM FORMA DE STRING SEM NENHUMA SEPARAÇÃO---------------
+        alert(arrayCardNumber.join(''));
+    }
 };
 
 export default validator;
