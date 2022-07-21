1360d462214d8121c738bdecf6fc47cf6640d889
Clarissa Simplício <clasimplicio@hotmail.com>
2022-07-21 20:21:01 -0300

    todo funcionando mas com css incompleto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import validator from './validator.js';
 
 //--------------ADICIONANDO EVENTO AO BOTÃO VALIDAR-----------------
+let creditCardNumber;
 document.getElementById("ok").addEventListener("click",pegar);
 
-let creditCardNumber;
 //--------------FUNÇÃO DO BOTÃO VALIDAR---------------------------------
 function pegar() {
     //-------------------RECEBE O Q FOI DIGITADO NO INPUT---------------
     creditCardNumber = document.getElementById("card").value;
-    //-----------CHAMANDO O MÉTODO DA OBJETO VALIDATOR---------------------
-    let valid = validator.isValid(creditCardNumber);
-    //-------------------------VERIFICANDO A VALIDAÇÃO COM O RETURNO NA VARIÁVEL VALID-----------------------
-    if(valid==true){
-        alert("CARTÃO VÁLIDO")
-    }else{
-        alert("CARTÃO INVÁLIDO")
+    if(creditCardNumber!=""){  
+        //-----------CHAMANDO O MÉTODO DA OBJETO VALIDATOR---------------------
+        let valid = validator.isValid(creditCardNumber);
+        //-------------------------VERIFICANDO A VALIDAÇÃO COM O RETURNO NA VARIÁVEL VALID-----------------------
+        if(valid==true){
+            alert("CARTÃO VÁLIDO")
+        }else{
+            alert("CARTÃO INVÁLIDO")
+        }
     }
 }
 
